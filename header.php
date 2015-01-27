8a58fe1dbd0f22edd522c4bba43798569f655b2a
Cesar Tapasco <cethapgames@gmail.com>
2015-01-26 21:59:34 -0500

    Haber que pasa sin el bootstrao viejo

--- header.php
+++ header.php
@@ -22,8 +22,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo("template_url"); ?>/styles/stylecherry.css">
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo("template_url"); ?>/styles/bootstrap.css">
-<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo("template_url"); ?>/styles/responsive.css">
+<!--link rel="stylesheet" type="text/css" media="all" href="<?php //bloginfo("template_url"); ?>/styles/bootstrap.css">
+<link rel="stylesheet" type="text/css" media="all" href="<?php //bloginfo("template_url"); ?>/styles/responsive.css"-->
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo("template_url"); ?>/bootstrap/bootstrap.min.css">
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo("template_url"); ?>/styles/flexslider.css">
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo("template_url"); ?>/style.css">
 <link rel="stylesheet" id="cherry-plugin-css" href="<?php bloginfo("template_url"); ?>/styles/cherry-plugin.css" type="text/css" media="all">
